7fa09e44e2a898e22a075ce3993b2f3af001a384
vjds-git <vjain0187@gmail.com>
2026-07-01 05:09:58 +0000

    Save before pull

--- project/projecttest.py
+++ project/projecttest.py
@@ -1,7 +1,6 @@
-python -c "
+
 import ast
 tree = ast.parse(open('project_starter.py').read())
 for node in ast.walk(tree):
     if isinstance(node, ast.FunctionDef):
         print(f'{node.name}  (line {node.lineno})')
-"
